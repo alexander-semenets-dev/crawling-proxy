36dfabbd23ab253e35469e7f8efd96ac474de58f
Alexander Semenets <alexander.semenets.dev@gmail.com>
2019-02-20 15:32:36 +0200

    Improve proxy server

---
+++ src/main/java/com/personal/crawling/proxy/CrawlingException.java
@@ -0,0 +1,8 @@
+package com.personal.crawling.proxy;
+
+public class CrawlingException extends RuntimeException {
+
+    public CrawlingException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/personal/crawling/proxy/ProxyServer.java
+++ src/main/java/com/personal/crawling/proxy/ProxyServer.java
@@ -1,67 +1,103 @@
 package com.personal.crawling.proxy;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Route;
 import spark.Spark;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Optional;
+import java.text.MessageFormat;
+
+import static java.util.Objects.isNull;
 
 public class ProxyServer {
 
     private final static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0";
 
     private static final OkHttpClient httpClient = new OkHttpClient();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final Logger logger = LoggerFactory.getLogger(ProxyServer.class);
 
     public static void main(String[] args) {
         Spark.get("/crawl-link", crawlLink());
+        Spark.exception(CrawlingException.class, (exception, request, response) -> {
+            response.status(500);
+            response.body(exception.getMessage());
+        });
     }
 
     private static Route crawlLink() {
         return (request, response) -> {
-            final Map<String, String> parameters = request.params();
-            final String crawlingUrl = parameters.get("url");
+            final String crawlingUrl = request.queryParams("url");
 
-            return getCrawledContentJsonObject(crawlingUrl).toString();
+            return getCrawledContentJsonObject(crawlingUrl);
         };
     }
 
-    private static ObjectNode getCrawledContentJsonObject(String crawlingUrl) throws IOException {
+    private static byte[] getCrawledContentJsonObject(String crawlingUrl) {
         final okhttp3.Request request = new okhttp3.Request.Builder()
                 .url(crawlingUrl)
                 .header("User-Agent", USER_AGENT)
                 .get()
                 .build();
 
-        final okhttp3.Response response = httpClient.newCall(request)
-                .execute();
+        try {
+            final okhttp3.Response response = httpClient.newCall(request)
+                    .execute();
 
-        final ObjectNode objectNode = objectMapper.createObjectNode()
-                .put("statusCode", response.code())
-                .put("statusMessage", response.message());
+            throwCrawlingExceptionIfResponseNotSuccessful(crawlingUrl, response);
 
+            return getResponseBodyBytes(response);
+        } catch (IOException e) {
+            final String errorMessage = MessageFormat.format(
+                    "Cannot crawl url {0}: {1}",
+                    crawlingUrl,
+                    e.getMessage()
+            );
+
+            throw new CrawlingException(errorMessage);
+        }
+    }
+
+    private static void throwCrawlingExceptionIfResponseNotSuccessful(
+            String crawlingUrl,
+            Response response
+    ) {
         if (response.isSuccessful()) {
-            final byte[] contentBytes = Optional.ofNullable(response.body())
-                    .map(responseBody -> {
-                        try {
-                            return responseBody.bytes();
-                        } catch (IOException e) {
-                            return new byte[0];
-                        }
-                    })
-                    .orElse(new byte[0]);
-
-            objectNode.put("contentBytes", contentBytes);
+            return;
         }
 
-        return objectNode;
+        final String errorMessage = MessageFormat.format(
+                "Cannot crawl url {0}: status code - {1}, status message - {2}",
+                crawlingUrl,
+                response.code(),
+                response.message()
+        );
+
+        tryCloseResponse(response);
+
+        throw new CrawlingException(errorMessage);
+    }
+
+    private static byte[] getResponseBodyBytes(Response response) throws IOException {
+        if (isNull(response.body())) {
+            return new byte[0];
+        }
+
+        final byte[] responseBodyBytes = response.body().bytes();
+
+        tryCloseResponse(response);
+
+        return responseBodyBytes;
+    }
+
+    private static void tryCloseResponse(Response response) {
+        try {
+            response.close();
+        } catch (Exception e) {
+            logger.error("Cannot close response", e);
+        }
     }
 
 }
